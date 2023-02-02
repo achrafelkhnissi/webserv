b92263bd0220721bca0212b0641c06397c9d05c5
Ismail Ait bella <iait-bel@e2r4p3.1337.ma>
2023-02-02 21:04:42 +0100

    add more tokens & line and offset for debuging

--- srcs/lib/toml/data/tokenizer/token/Token.hpp
+++ srcs/lib/toml/data/tokenizer/token/Token.hpp
@@ -6,13 +6,19 @@ class Token {
 
 public:
 	enum e_token {
-		TK_KEY = 1,
-		TK_ASSIGN = 1 << 1,
-		TK_VALUE = 1 << 2,
-		TK_EOF = 1 << 4,
+		KEY = 1,
+		ASSIGN = 1 << 1,
+		VALUE = 1 << 2,
+		DOT = 1 << 3,
+		OpenBracket = 1 << 4,
+		CloseBracket = 1 << 5,
+		COMMENT = 1 << 8,
+		_EOF = 1 << 30,
 	};
 	enum e_token type;
 	std::string value;
+	int line;
+	int offset;
 
 	bool is(enum e_token type) {
 		return this->type | type;
