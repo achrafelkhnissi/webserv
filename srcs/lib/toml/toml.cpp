344a5e8e5f394895ca6dcfb98270b9d4cbe2b634
Ismail Ait bella <iait-bel@e2r1p5.1337.ma>
2023-02-03 22:47:53 +0100

    chore: recieve token properly from Lexer

--- srcs/lib/toml/toml.cpp
+++ srcs/lib/toml/toml.cpp
@@ -1,5 +1,7 @@
 #include "toml.hpp"
 #include "data/table.hpp"
+#include "tokenizer/Lexer.hpp"
+#include "tokenizer/token/Token.hpp"
 #include <fstream>
 #include <iostream>
 #include <istream>
@@ -7,17 +9,28 @@
 
 using namespace toml;
 
-table* toml::parse_stream(std::ifstream& is) {
-	(void)is;
-	table* t = new table();
-
-	return t;
+table* toml::parse_stream(std::ifstream& in) {
+	Lexer lexer = Lexer(in);
+	Result<Token*, ParseError> r;
+	
+	do {
+		r = lexer.next();
+		if (r.is_ok())
+			cout << r.ok()->as_str() << endl;
+		else
+		{
+			cerr << r.err().as_str() << endl;
+			break;
+		}
+	}
+	while (!r.ok()->is(Token::_EOF));
+	return new toml::table();
 }
 
 table* toml::parse_file(std::string& filename) {
 	std::ifstream ifs(filename);
 
-	if (ifs.is_open())
+	if (!ifs.is_open())
 		return NULL;
 	return parse_stream(ifs);
 }
