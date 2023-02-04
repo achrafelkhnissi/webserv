d0a2a47b611b9c4e2881828a0ee874ebb5aee9f6
Ismail Ait bella <iait-bel@e2r1p2.1337.ma>
2023-02-04 23:50:05 +0100

    chore: had to goo hhhh

--- srcs/lib/toml/data/table.cpp
+++ srcs/lib/toml/data/table.cpp
@@ -1,9 +1,35 @@
 #include "table.hpp"
+#include <string>
+#include <utility>
 
-toml::table::table() {
-	value = "test";
+using namespace toml;
+
+table::table(enum e_toml type) {
+	this->type = type;
 }
 
-toml::table::~table() {
-	
+table::table(TomlMap& mp) {
+	this->mp = mp;
+	this->type = TABLE;
 }
+
+table::table(std::string& str)
+	: type(STRING)
+	, str(str) { }
+
+void* table::operator[](std::string& idx) {
+	TomlMap::iterator f = mp.find(idx);
+	if (f == mp.end())
+		return NULL;
+	return f->second;
+}
+
+void table::push(table t) {
+	vec.push_back(t);
+}
+
+void table::insert(std::string s, void* t) {
+	mp.insert(std::make_pair(s, t));
+}
+
+table::~table() { }
