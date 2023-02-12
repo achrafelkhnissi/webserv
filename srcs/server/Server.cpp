43e99aa845ce94ab604b3a737ffef2976aa40bb9
Fathiya Jamia <fathiyajamaa051@gmail.com>
2023-02-12 15:05:15 +0100

    add the poll() function

--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -2,12 +2,20 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <poll.h>
+#include <fcntl.h>
 
-Server::Server(int port, const std::string& host) : _port(port), _host(host) { }
+#define  FALSE 1
+#define  TRUE 0
+Server::Server(int port, const std::string &host): _port(port), _host(host) { }
 
-void Server::start() {
+void Server::set_up() {
+
+    pollfd listner_fd;
     // Create a socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    fcntl(sockfd, F_SETFL, O_NONBLOCK);
 
     // Bind the socket to a port and host
     struct sockaddr_in server_address = {};
@@ -26,39 +34,121 @@ void Server::start() {
         exit(EXIT_FAILURE);
     }
 
+    listner_fd.fd = sockfd;
+    listner_fd.events = POLLIN;
+    _fds.push_back(listner_fd);
     // handle_connection(sockfd);
 
-    handle_request(sockfd);
+//    handle_request(sockfd);
+    start_polling(sockfd);
 }
 
-void Server::handle_connection(int sockfd) {
+void Server::start_polling(int listener_socket) {
+    int timeout = 60 * 1000; // 1 min for now
+    int r;
+    do{
+        std::cout << "waiting on poll..." << std::endl;
+        r = poll(_fds.data(), _fds.size(), timeout);
+
+        if (r < 0){
+            perror(" poll system call failed");
+            break;
+        }
+        if (r == 0){
+            std::cout << "poll() timed out.";
+            break;
+        }
+        //loop through to find the descriptors that returned
+
+        std::vector<pollfd>::iterator it = _fds.begin();
+        std::vector<pollfd>::iterator end = _fds.end();
+//        std::vector<pollfd>::iterator it;
+        for(; it != end; it++){
+            if (it->revents == 0)
+                continue;
+            if (it->revents != POLLIN){
+                perror(" Error! revents ");
+                break;
+            }
+            if (it->fd == listener_socket){
+                std::cout << "listener socket is readable" << std::endl;
+                //call the handle_connections() to accept all the incoming connections queued up in the listening socket then call pall again
+                handle_connections( listener_socket);
+            }
+            else
+                // This is not the listening socket, therefore an
+                // existing connection must be readable
+                handle_request(it);
+        }
+    } while (true);
+
+}
+
+void Server::handle_connections(int sockfd) {
     // Accept an incoming connection
-    int new_fd = accept(sockfd, nullptr, nullptr);
-    std::cout << "Connection accepted." << std::endl;
+    int new_fd;
+    do {
+        new_fd = accept(sockfd, nullptr, nullptr);
+        /* accept each incomming connection if accept fails with EWOULDBLOCK, then
+         * we have accepted all of them, else we need to end the server ( to do later) */
+
+        if (new_fd < 0)
+        {
+            perror("accept failed!");
+            break;
+        }
+        std::cout << "Connection accepted." << std::endl;
+        pollfd connection;
+        connection.fd = new_fd;
+        connection.events = POLLIN;
+        _fds.push_back(connection);
+    } while (new_fd != -1);
 }
 
-void Server::handle_request(int fd) {
+void Server::handle_request(std::vector<pollfd>::iterator it) {
 
-    int new_fd = accept(fd, nullptr, nullptr); // remove this line later.
+//    int new_fd = accept(fd, nullptr, nullptr); // remove this line later.
 
     // Read the incoming request
+    int close_conn = FALSE;
     char buffer[1024];
-    int bytes_read = recv(new_fd, buffer, sizeof(buffer), 0);
+    do {
+        /* receive data in this connection until the rcv fails with EWOULDBLOCK
+         * if any other failure occurs, we will close the connetion*/
 
-    // Parse the request and call the appropriate method
-    std::string request(buffer, bytes_read);
-    std::string method = request.substr(0, request.find(" "));
+        int bytes_read = recv(it->fd, buffer, sizeof(buffer), 0);
+        if (bytes_read < 0)
+        {
+            if (errno != EWOULDBLOCK)
+                close_conn = TRUE;
+            perror("recv failed!");
+            break;
+        }
+        if (bytes_read == 0){ // check to see if the connection has been closed by the client
+            std::cout << "Connection closed!";
+            close_conn = TRUE;
+            break;
+        }
 
-    printf("Request:\n\n%s\n", request.c_str());
+        // Parse the request and call the appropriate method
+        std::string request(buffer, bytes_read);
+        std::string method = request.substr(0, request.find(" "));
 
-    if (method == "GET") {
-        handle_get(new_fd);
-    } else if (method == "POST") {
-        handle_post(new_fd);
-    } else if (method == "DELETE") {
-        handle_delete(new_fd);
-    } else {
-        handle_error(new_fd);
+        printf("Request:\n\n%s\n", request.c_str());
+
+        if (method == "GET") {
+            handle_get(it->fd);
+        } else if (method == "POST") {
+            handle_post(it->fd);
+        } else if (method == "DELETE") {
+            handle_delete(it->fd);
+        } else {
+            handle_error(it->fd);
+        }
+    }while(true);
+    if (close_conn){
+        close(it->fd);
+        _fds.erase(it);
     }
 }
 
--- srcs/server/Server.hpp
+++ srcs/server/Server.hpp
@@ -10,16 +10,17 @@ class Server {
 private:
     int         _port; // port number
     std::string _host; // hostname
-    // std::vector<pollfd> _fds; // vector to store file descriptor information
+    std::vector<pollfd> _fds; // vector to store file descriptor information
 
 public:
     Server(int port, const std::string& host); // constructor that takes port and host as an argument
     ~Server();
 
-    void start(); // method to start the server, it will listen for incoming connections
+    void set_up(); // method to start the server, it will listen for incoming connections
+    void start_polling(int listener_socket); // waiting for any I/O operation on the _fds using the poll function
 
-    void handle_connection(int fd); // method to handle incoming connections, it will accept new connections and add them to the poll list
-    void handle_request(int fd); // method to handle incoming requests, it will read the request, parse it and call the appropriate method
+    void handle_connections(int fd); // method to handle incoming connections, it will accept new connections and add them to the poll list
+    void handle_request(std::vector<pollfd>::iterator it); // method to handle incoming requests, it will read the request, parse it and call the appropriate method
     void send_response(int fd); // method to send HTTP responses, it will send the response to the client
     void handle_get(int fd); // method to handle GET requests
     void handle_post(int fd); // method to handle POST requests
