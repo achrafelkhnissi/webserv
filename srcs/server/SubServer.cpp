c91f0ae1513283483099562371e6846e5e225c3f
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-18 13:36:53 +0100

    ADDED: Adding sub-server class files

--- srcs/server/SubServer.cpp
+++ srcs/server/SubServer.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "SubServer.h"
+
+SubServer::SubServer() {
+
+}
+
+SubServer::~SubServer() {
+
+}
+
+const std::vector<std::string>& SubServer::getServerName() const {
+    return _server_name;
+}
+
+const std::string& SubServer::getRoot() const {
+    return _root;
+}
+
+const std::string& SubServer::getIndex() const {
+    return _index;
+}
+
+const std::string& SubServer::getErrorPages() const {
+    return _error_pages;
+}
+
+const std::string& SubServer::getClientMaxBodySize() const {
+    return _client_max_body_size;
+}
+
+const location& SubServer::getLocation() const {
+    return _location;
+}
