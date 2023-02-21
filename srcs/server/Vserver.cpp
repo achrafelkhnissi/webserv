39da7e4effc2709c266d71b54191dc15a7b8e695
Fathiya Jamia <fathiyajamaa051@gmail.com>
2023-02-21 09:49:59 +0100

    UPDATE: moving the get methods and start server to server.cpp

--- srcs/server/Vserver.cpp
+++ srcs/server/Vserver.cpp
@@ -4,297 +4,56 @@
 
 #include "Vserver.hpp"
 
-/*
- * Vserver class - This class is responsible for creating a virtual server object and start the server
- *
- * @param port - port number
- * @param host - hostname
- */
-Vserver::Vserver(const std::pair<std::string , int >&  host_port): _host_port(host_port) {
+Vserver::Vserver(ServerConfig server_config) {
+
+    _host_port = std::make_pair(server_config.host, server_config.port);
+        this->addSubServer(server_config);
     std::cout << "\nCreating a virtual server on "
               << "port '" << _host_port.second
               << "' and host '"<< _host_port.first << "'\n" << std::endl;
-    _setup();
 }
 
-/*
- * Setup method - This method is responsible for creating a socket, binding it to a port
- *              and host and listening for incoming connections
- */
-void Vserver::_setup() {
-    // Create a socket
-    int _server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (_server_socket == -1)
-        _error("socket");
 
-    // Set the server socket to non-blocking mode
-    if (fcntl(_server_socket, F_SETFL, O_NONBLOCK) < 0)
-        _error("fcntl");
-
-    // Set the server socket to reuse the address
-    int optval = 1;
-    if (setsockopt(_server_socket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
-        _error("setsockopt");
-
-    // Bind the socket to the port and host
-    struct sockaddr_in server_addr = {};
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(_host_port.second);
-
-    if (bind(_server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
-        strerror(errno);
-
-    // Listen for incoming connections
-    if (listen(_server_socket, SOMAXCONN) == -1)
-        _error("listen");
-
-    // Add the server socket to the pollfds list
-    struct pollfd server_fd = {};
-    server_fd.fd = _server_socket;
-    server_fd.events = POLLIN;
-    _fds.push_back(server_fd);
-}
-
-/*
- * Start method - This method is responsible for starting the server and waiting for incoming connections and requests
- * 				using the poll function
- */
-void Vserver::start() {
-
-    int timeout = 60 * 1000; // 60 seconds
-
-    // Setting up the server
-    _setup();
-
-    // Start the server
-    while (true) {
-        int ready = poll(_fds.data(), _fds.size(), timeout);
-        if (ready == -1)
-            _error("poll");
-
-        if (ready == 0)
-            _error("poll timeout");
-
-        if ( _fds[0].revents & POLLIN ) {
-
-            _handle_connections(_fds[0].fd);
-        }
-
-        for (pollfds_it it = _fds.begin() + 1; it != _fds.end(); ++it) {
-            if (it->revents & POLLIN) {
-                _handle_request(it);
-            }
-        }
-
-    }
-//    _clear_pollfds();
-}
-
-/*
- * Handle connections method - This method is responsible for handling incoming connections
- * 							and adding them to the pollfds list
- */
-void Vserver::_handle_connections(int sockfd) {
-    // Accept an incoming connection
-
-    while (true) {
-        struct sockaddr_in client_addr = {};
-        socklen_t client_len = sizeof(client_addr);
-        int client_fd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
-        if (client_fd == -1) {
-            if (errno == EWOULDBLOCK || errno == EAGAIN) {
-                break;
-            } else {
-                _error("accept");
-            }
-        }
-
-        // Add the client socket to the pollfds list
-        struct pollfd client = {};
-        client.fd = client_fd;
-        client.events = POLLIN;
-        _fds.push_back(client);
-
-		std::cout << "Accepted connection on fd " << client_fd << std::endl;
-    }
-}
-
-/*
- * Handle request method - This method is responsible for handling incoming requests
- * 							and sending a response to the client
- */
-void Vserver::_handle_request(pollfds_it it) {
-    // Handle the request
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read = recv(it->fd, buffer, BUFFER_SIZE, 0);
-
-    if (bytes_read == -1)
-        _error("recv");
-
-    if (bytes_read == 0) {
-        std::cout << "Closed connection on fd " << it->fd << std::endl;
-        close(it->fd);
-        _fds.erase(it);
-        return;
-    }
-
-    buffer[bytes_read] = '\0';
-    std::cout << "Received " << bytes_read << " bytes on fd " << it->fd << std::endl;
-    std::cout << buffer << std::endl;
-
-	// Send the response to the client
-	_handle_get(it->fd);
-}
-
-/*
- * _clear_pollfds method - This method is responsible for clearing the pollfds list
- * 							and closing all the sockets
- */
-void Vserver::_clear_pollfds() {
-    pollfds_it it = _fds.begin();
-    pollfds_it end = _fds.end();
-
-    for(; it != end; it++)
-        close(it->fd);
-    _fds.clear();
-}
-
-/*
- * _send_response method - This method is responsible for sending a response to the client
- * 							after handling the request
- */
-void Vserver::_send_response(int fd) {
-// Send the response to the client
-    std::string response = "HTTP/1.1 200 OK\r\n"
-                           "Content-Type: text/html\r\n\r\n"
-                           "<h1>Hello World!</h1>";
-    send(fd, response.c_str(), response.length(), 0);
-}
-
-/*
- * _handle_get method - This method is responsible for handling GET requests
- * 							and sending a response to the client
- */
-void Vserver::_handle_get(int fd) {
-
-	std::string _root = "www/";
-	std::string _resource = "index.html";
-
-	std::string resource_path = _root + _resource;
-
-	std::ifstream resource_file;
-	std::stringstream response_header_stream;
-	std::string response_header;
-	std::string response_body;
-	int content_length, response_size, bytes_sent;
-
-	// open the requested resource file
-	resource_file.open(resource_path, std::ios::binary);
-	if (!resource_file.is_open()) {
-		// if the file does not exist, send a 404 response
-		response_header_stream << "HTTP/1.1 404 Not Found\r\n\r\n";
-		response_header = response_header_stream.str();
-		send(fd, response_header.c_str(), response_header.length(), 0);
-		return;
-	}
-
-	// determine the size of the resource file
-	resource_file.seekg(0, std::ios::end);
-	content_length = resource_file.tellg();
-	resource_file.seekg(0, std::ios::beg);
-
-	// allocate memory for the response body and read the resource file contents into it
-	response_body.resize(content_length);
-	resource_file.read(&response_body[0], content_length);
-
-	// construct the response header with the content length and send it to the client
-	response_header_stream << "HTTP/1.1 200 OK\r\nContent-Length: " << content_length << "\r\n\r\n";
-	response_header = response_header_stream.str();
-	send(fd, response_header.c_str(), response_header.length(), 0);
-
-	// send the response body to the client in chunks
-	bytes_sent = 0;
-	while (bytes_sent < content_length) {
-		response_size = send(fd, &response_body[bytes_sent], content_length - bytes_sent, 0);
-		if (response_size < 0) {
-			// error sending response to client
-			break;
-		}
-		bytes_sent += response_size;
-	}
-//    _send_response(fd);
-}
-
-/*
- *  @disc _handle_post method - This method is responsible for handling POST requests
- * 							and sending a response to the client
- * 	@param fd: the file descriptor of the client socket
- */
-void Vserver::_handle_post(int fd) {
-    // Handle the POST request
-    // ...
-    (void)fd;
-}
-
-/*
- * @disc _handle_delete method - This method is responsible for handling DELETE requests
- * @param fd: the file descriptor of the client socket
- */
-void Vserver::_handle_delete(int fd) {
-    // Handle the DELETE request
-    // ...
-    (void)fd;
-}
-
-/*
- * @disc _handle_error method - This method is responsible for handling errors
- *
- * @param fd: the file descriptor of the client socket
- */
-void Vserver::_handle_error(int fd) {
-    // Handle the error
-    // ...
-    (void)fd;
-
-	// TODO: Structure to handle errors (not completed yet)
-	int status_code = 200;
-	switch (status_code) {
-	case 200:
-		std::cout << "HTTP/1.1 200 OK\r\n\r\n";
-		std::cout << "<html><body><h1>Hello, world!</h1></body></html>";
-		break;
-	case 403:
-		std::cout << "HTTP/1.1 403 Forbidden\r\n\r\n";
-		std::cout << "<html><body><h1>403 Forbidden</h1></body></html>";
-		break;
-	case 404:
-		std::cout << "HTTP/1.1 404 Not Found\r\n\r\n";
-		std::cout << "<html><body><h1>404 Not Found</h1></body></html>";
-		break;
-	case 500:
-		std::cout << "HTTP/1.1 500 Internal Server Error\r\n\r\n";
-		std::cout << "<html><body><h1>500 Internal Server Error</h1></body></html>";
-		break;
-	default:
-		std::cout << "HTTP/1.1 400 Bad Request\r\n\r\n";
-		std::cout << "<html><body><h1>400 Bad Request</h1></body></html>";
-		break;
-    }
-}
-
-Vserver::~Vserver() {
-    // Close all open file descriptors
-}
-
-/*
- * _error() - prints an error message and exits the program with EXIT_FAILURE
- *
- * @param msg: the error message to print
- */
 void Vserver::_error(const std::string &msg) {
 //    strerror(errno);
     std::cerr << msg << std::endl;
     exit(EXIT_FAILURE);
+}
+Vserver::~Vserver() {
+    // Close all open file descriptors
+}
+
+
+void Vserver::addSubServer(ServerConfig &config) {
+    _sub_servers.push_back(SubServer(config));
+}
+
+Vserver::Vserver() {
+
+}
+
+int Vserver::getServerFd() const {
+    return _server_fd;
+}
+
+void Vserver::print_data() const {
+    std::cout << "--------------------------------------------" << std::endl;
+    std::cout << "Server fd: " << _server_fd << std::endl;
+    std::cout << "Host: " << _host_port.first << std::endl;
+    std::cout << "Port: " << _host_port.second << std::endl;
+    // print sub servers
+    std::cout << "Sub servers: " << std::endl;
+    for (vector<SubServer>::const_iterator it = _sub_servers.begin(); it != _sub_servers.end(); ++it) {
+        it->print_data();
+    }
+    std::cout << "--------------------------------------------" << std::endl;
+    std::cout << std::endl;
+}
+
+int Vserver::getPort() const {
+    return _host_port.second;
+}
+
+const std::string& Vserver::getRoot() const {
+    return _sub_servers[0].getRoot();
 }
--- srcs/server/Vserver.hpp
+++ srcs/server/Vserver.hpp
@@ -17,69 +17,44 @@
 #include <fstream>
 #include <sstream>
 
-#include "SubServer.h"
+#include "SubServer.hpp"
+#include "../config/Configuration.hpp"  // Configuration class
+
 
 #define BUFFER_SIZE 1024
 
-/*
- * TODO:
- * 	1. Modify the vsever class to stop only the default server with the IP:PORT and a vector of sub-servers
- */
-
-/*
- * Vserver class - This class is responsible for creating a virtual server object
- *
- * @var _port - port number
- * @var _end_server - boolean to check if the server is running or not
- * @var _host - hostname
- * @var _fds - vector to store file descriptor information
- *
- * @method Vserver - constructor that takes port and host as an argument
- * @method ~Vserver - destructor
- * @method setup - method to start the server, it will listen for incoming connections
- * @method start_polling - waiting for any I/O operation on the _fds using the poll function
- * @method clear_pollfds - method to clear the pollfds
- * @method handle_connections - method to handle incoming connections, it will accept new connections and add them to the poll list
- * @method handle_request - method to handle incoming requests, it will read the request, parse it and call the appropriate method
- * @method send_response - method to send HTTP responses, it will send the response to the client
- * @method handle_get - method to handle GET requests
- * @method handle_post - method to handle POST requests
- * @method handle_delete - method to handle DELETE requests
- * @method handle_error - method to handle errors
- */
 class Vserver {
 
 private:
-    typedef std::vector<pollfd>                 pollfds;
-    typedef std::vector<pollfd>::iterator       pollfds_it;
+
 	typedef std::pair<std::string, int>			host_port;
+    typedef std::vector<SubServer>              subServers;
 
 private:
 
-	host_port 			_host_port;
+    host_port 			_host_port;
+    int                 _server_fd;
 
-    pollfds             _fds;
-	SubServer           _sub_servers;	// for handling multiple virtual servers
+	subServers          _sub_servers;	// for handling multiple virtual servers
 
-
-private:
-    void _setup();
-    void _clear_pollfds();
-    void _error(const std::string& msg);
-
-    void _handle_connections(int fd);
-    void _handle_request(pollfds_it it);
-    void _send_response(int fd);
-    void _handle_get(int fd);
-    void _handle_post(int fd);
-    void _handle_delete(int fd);
-    void _handle_error(int fd);
 
 public:
-   Vserver(const std::pair<std::string , int >&  host_port);
+    void _setup();
+    void _error(const std::string& msg);
+    void start();
+
+
+public:
+    Vserver();
+    Vserver(ServerConfig config);
     ~Vserver();
 
-    void start();
+    int getServerFd() const;
+    void addSubServer(ServerConfig &config);
+    void print_data() const;
+    int getPort() const;
+    const std::string& getRoot() const;
+
 };
 
 #endif //WEBSERV_VSERVER_HPP
