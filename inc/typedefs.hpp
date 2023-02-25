69d17fdab4748044bd972e9f46439f796ae9563e
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-25 18:39:29 +0100

    Moving typdefs to one location

--- inc/typedefs.hpp
+++ inc/typedefs.hpp
@@ -1,8 +1,53 @@
-//
-// Created by Achraf El khnissi on 2/25/23.
-//
-
 #ifndef WEBSERV_TYPEDEFS_HPP
 #define WEBSERV_TYPEDEFS_HPP
 
+#include <string>
+#include <map>
+#include <vector>
+#include <poll.h>
+#include <iostream>
+
+
+class SubServer;
+class Vserver;
+class Server;
+
+typedef struct location_s location_t;
+
+/* Server Typdefs */
+typedef std::vector<pollfd>                         pollfdsVector_t;
+typedef std::vector<pollfd>::iterator               pollfdsVectorIterator_t;
+typedef std::vector<pollfd>::const_iterator         pollfdsVectorConstIterator_t;
+
+typedef std::map<int, Vserver>                      virtualServerMap_t;
+typedef virtualServerMap_t::iterator                virtualServerMapIterator_t ;
+typedef virtualServerMap_t::const_iterator          virtualServerMapConstIterator_t;
+
+typedef std::map<int, std::string>			        errorPagesMap_t;
+typedef errorPagesMap_t::iterator			        errorPagesMapIterator_t;
+typedef errorPagesMap_t::const_iterator		        errorPagesMapConstIterator_t;
+
+/* SubServer Typdefs */
+typedef std::vector<location_t>                       locationVector_t;
+typedef std::vector<location_t>::iterator             locationVectorIterator_t;
+typedef std::vector<location_t>::const_iterator       locationVectorConstIterator_t;
+
+/* VirtualServer Typdefs */
+typedef std::pair<std::string, int>			            hostPortPair_t;
+
+typedef std::vector<SubServer>                      subServerVector_t;
+typedef std::vector<SubServer>::iterator            subServersIterator_t;
+typedef std::vector<SubServer>::const_iterator      subServersConstIterator_t;
+
+
+/* Utils Typdefs */
+typedef std::vector<std::string>                    stringVector_t;
+typedef std::vector<std::string>::iterator          stringVectorIterator_t;
+typedef std::vector<std::string>::const_iterator    stringVectorConstIterator_t;
+
+#include "Server.hpp"
+#include "Vserver.hpp"
+#include "SubServer.hpp"
+
 #endif //WEBSERV_TYPEDEFS_HPP
+
