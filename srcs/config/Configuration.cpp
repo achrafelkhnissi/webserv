5dc4b4dcc4ca4872cf022c602eb285a6c0308a2b
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-02-28 11:08:31 +0100

    EDIT: Changes to the default values

--- srcs/config/Configuration.cpp
+++ srcs/config/Configuration.cpp
@@ -8,16 +8,16 @@ LocationConfig fill_location(toml::table& location) {
 	LocationConfig l;
 
 	l.path = location["path"].as_str("/");
-	l.root = location["root"].as_str("/var/www/html");
+	l.root = location["root"].as_str("www");
 	l.index = location["index"].as_str("index.html");
 	l.autoindex = location["autoindex"].as_str("off");
-	l.upload_path = location["upload_path"].as_str("/var/www/html/upload");
+	l.upload_path = location["upload_path"].as_str("www/html/upload");
 	l.upload_store = location["upload_store"].as_str("on");
-	l.cgi_path = location["cgi_path"].as_str("/var/www/html/cgi");
+	l.cgi_path = location["cgi_path"].as_str("www/html/cgi");
 	l.cgi_extension = location["cgi_extension"].as_str(".cgi");
 	l.cgi_pass = location["cgi_pass"].as_str("");
 	l.client_max_body_size = location["_clientMaxBodySize"].as_str("10m");
-	l.error_page = location["error_page"].as_str("404 /404.html");
+	l.error_page = location["error_page"].as_str("404 /errors/error-404.html");
 	return l;
 }
 
@@ -26,10 +26,10 @@ ServerConfig fill_server(toml::table& server) {
 	s.port = server["port"].as_int(s.port);
 	s.host = server["host"].as_str("127.0.0.1");
 	ITER_FOREACH(vector<toml::table>, server["server_name"].vec, it) {
-		s.server_name.push_back(it->as_str("default.com"));
+		s.server_name.push_back(it->as_str("localhost"));
 	}
-	s.root = server["root"].as_str("/var/www/html");
-	s.error_page = server["error_page"].as_str("404 /404.html");
+	s.root = server["root"].as_str("www");
+	s.error_page = server["error_page"].as_str("404 /errors/error-404.html");
 	s.client_max_body_size = server["_clientMaxBodySize"].as_str("10m");
 
 	toml::table& t = server["location"];
