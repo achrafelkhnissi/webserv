04b5f191882e649bb1f6f054a68e4db72f74ccf0
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-05 09:38:37 +0100

    add: CGIHandler

---
+++ srcs/CGIHandler/CGIHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Achraf El khnissi on 3/4/23.
+//
+
+#include "CGIHandler.hpp"
---
+++ srcs/CGIHandler/CGIHandler.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Achraf El khnissi on 3/4/23.
+//
+
+#ifndef WEBSERV_CGIHANDLER_HPP
+#define WEBSERV_CGIHANDLER_HPP
+
+
+class CGIHandler {
+
+};
+
+
+#endif //WEBSERV_CGIHANDLER_HPP
