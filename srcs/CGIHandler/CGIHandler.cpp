6011ad87f208b6970b305acdc05cd2cadfcb5bb3
Fathiya Jamia <fathiyajamaa051@gmail.com>
2023-03-05 16:29:17 +0100

    feat: add getCmd() function to find the suitable executable based on the PATH_INFO

--- srcs/CGIHandler/CGIHandler.cpp
+++ srcs/CGIHandler/CGIHandler.cpp
@@ -3,3 +3,78 @@
 //
 
 #include "CGIHandler.hpp"
+#include <algorithm>
+
+
+
+CGIHandler::CGIHandler(const stringMap_t &env) {
+    _envSize = env.size();
+    _env = new char*[_envSize + 1];
+
+    int i = 0;
+    for(stringMap_t::const_iterator it = env.begin(); it != env.end(); it++, i++) {
+        _env[i] = strdup((it->first + "=" + it->second).c_str());
+    }
+
+    _envMap = env; // todo: remove this line
+}
+
+string CGIHandler::getCmd() {
+
+    string path_ = _envMap["PATH_INFO"];
+    string scriptName_ = path_.substr(path_.find_last_of('/') + 1);
+    string extension_ = ".py";
+    string cmd_ = "./www/cgi-bin/";
+
+
+    stringVectorIterator_t it = find(_cgiPath.begin(), _cgiPath.end(), scriptName_);
+    if (it != _cgiPath.end()) {
+        cout << "Found file with" << extension_ << " extension: " << *it << endl;
+       return  cmd_ += *it;
+    } else {
+        std::size_t pos = path_.find_last_of('.');
+        if (pos != std::string::npos) {
+            extension_ = path_.substr(pos);
+        }
+
+        for (stringVectorIterator_t it = _cgiPath.begin(); it != _cgiPath.end(); ++it) {
+            if (it->size() >= extension_.length() && it->substr(it->size() - extension_.length()) == extension_) {
+                cout << "Found file with" << extension_ << " extension: " << *it << endl;
+               return cmd_ += *it;
+            }
+        }
+    }
+    cout << "Found file with" << extension_ << " extension: " << cmd_ + "helloCGI.py" << endl;
+   return cmd_ += "helloCGI.py" ;
+}
+//void CGIHandler::CGIExecuter() const {
+//
+//    pid_t pid = fork();
+//    if (pid == -1) {
+//        throw std::runtime_error("fork() failed");
+//    } else if (pid == 0) {
+//        // child process
+//        if (execve(_cgiPath.c_str(), NULL, _env) == -1) {
+//            throw std::runtime_error("execve() failed");
+//        }
+//    } else {
+//        // parent process
+//        int status;
+//        waitpid(pid, &status, 0);
+//        if (WIFEXITED(status)) {
+//            std::cout << "Child exited with status " << WEXITSTATUS(status) << std::endl;
+//        } else if (WIFSIGNALED(status)) {
+//            std::cout << "Child terminated by signal " << WTERMSIG(status) << std::endl;
+//        }
+//    }
+//
+//}
+
+
+CGIHandler::~CGIHandler() {
+    for(int i = 0; i < _envSize; i++) {
+        free(_env[i]);
+    }
+    delete[] _env;
+}
+
--- srcs/CGIHandler/CGIHandler.hpp
+++ srcs/CGIHandler/CGIHandler.hpp
@@ -38,45 +38,56 @@ Server specific variables:
 - SERVER_NAME: host name of the server, may be dot-decimal IP address.
 - GATEWAY_INTERFACE: CGI/version.
 
-Request specific variables:
-- SERVER_PROTOCOL: HTTP/version.
-- SERVER_PORT: TCP port (decimal).
-- REQUEST_METHOD: name of HTTP method (see above).
-- PATH_INFO: path suffix, if appended to URL after program name and a slash.
-- PATH_TRANSLATED: corresponding full path as supposed by server, if PATH_INFO is present.
-- SCRIPT_NAME: relative path to the program, like /cgi-bin/script.cgi.
-- QUERY_STRING: the part of URL after ? character. The query string may be composed of *name=value pairs separated with ampersands (such as var1=val1&var2=val2...) when used to submit form data transferred via GET method as defined by HTML application/x-www-form-urlencoded.
-- REMOTE_HOST: host name of the client, unset if server did not perform such lookup.
-- REMOTE_ADDR: IP address of the client (dot-decimal).
-- AUTH_TYPE: identification type, if applicable.
-- REMOTE_USER used for certain AUTH_TYPEs.
-- REMOTE_IDENT: see ident, only if server performed such lookup.
-- CONTENT_TYPE: Internet media type of input data if PUT or POST method are used, as provided via HTTP header.
-- CONTENT_LENGTH: similarly, size of input data (decimal, in octets) if provided via HTTP header.
+TODO: Request specific variables:
+    - SERVER_PROTOCOL: HTTP/version.
+    - SERVER_PORT: TCP port (decimal).
+    - REQUEST_METHOD: name of HTTP method (see above).
+    - PATH_INFO: path suffix, if appended to URL after program name and a slash.
+    - PATH_TRANSLATED: corresponding full path as supposed by server, if PATH_INFO is present.
+    - SCRIPT_NAME: relative path to the program, like /cgi-bin/script.cgi.
+    - QUERY_STRING: the part of URL after ? character. The query string may be composed of *name=value pairs separated with ampersands (such as var1=val1&var2=val2...) when used to submit form data transferred via GET method as defined by HTML application/x-www-form-urlencoded.
+    - REMOTE_HOST: host name of the client, unset if server did not perform such lookup.
+    - REMOTE_ADDR: IP address of the client (dot-decimal).
+    - AUTH_TYPE: identification type, if applicable.
+    - REMOTE_USER used for certain AUTH_TYPEs.
+    - REMOTE_IDENT: see ident, only if server performed such lookup.
+    - CONTENT_TYPE: Internet media type of input data if PUT or POST method are used, as provided via HTTP header.
+    - CONTENT_LENGTH: similarly, size of input data (decimal, in octets) if provided via HTTP header.
 
 Variables passed by user agent (HTTP_ACCEPT, HTTP_ACCEPT_LANGUAGE, HTTP_USER_AGENT, HTTP_COOKIE and possibly others) contain values of corresponding HTTP headers and therefore have the same sense.
 
  *
+ * REMARKS:
+ *  - The file requested by the client is passed to the CGI program as the first argument. (URL does not map to a flat file, but instead somehow to a scripting area)
+ *  - The CGIPASS directive is used to specify the directory where the CGI programs are located.
+ *
+ *  http://example.com/cgi-bin/myprogram.cgi?file=index.html example of a CGI request
  */
+
+#include "../request/Request.hpp"
+//#include "../server/SubServer.hpp"
+#include "../../inc/typedefs.hpp"
 
 class CGIHandler {
 
-private:
-	string _method;
-	string _url;
-	string _headers;
-	string _cgiPath;
-	string _query;
+public: // make it private later
+	stringVector_t _cgiPath;
 	string _body;
-	string _response;
+
+    stringMap_t _envMap; // todo: delete this later
+    char **_env;
+    size_t _envSize;
 
 
 public:
-	CGIHandler(const string& path, const string& query, const string& body);
+//	CGIHandler(const string& path, const Request& request, const string& body);
+    CGIHandler(const stringMap_t& env);
 	~CGIHandler();
 
 	void handle();
 	const string& getResponse() const;
+    string getCmd();
+    void CGIExecuter() const;
 };
 
 
