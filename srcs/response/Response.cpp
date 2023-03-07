d12ee51d42497c20748aee3142091f7179656f2a
Fathiya Jamia <fathiyajamaa051@gmail.com>
2023-03-07 17:33:30 +0100

    feat: add response class, initial virsion

---
+++ srcs/response/Response.cpp
@@ -0,0 +1,80 @@
+#include "Response.hpp"
+
+Response::Response() {
+    statusCode = 200;
+    protocol = "HTTP";
+    version = "1.1";
+    body = "Hello World";
+
+    http_errors[400] = "Bad Request";
+    http_errors[401] = "Unauthorized";
+    http_errors[403] = "Forbidden";
+    http_errors[404] = "Not Found";
+    http_errors[405] = "Method Not Allowed";
+    http_errors[415] = "Unsupported Media Type";
+    http_errors[500] = "Internal Server Error";
+    http_errors[501] = "Not Implemented";
+    http_errors[503] = "Service Unavailable";
+}
+
+Response::~Response() {}
+
+void Response::setStatusCode(const string& filePath, std::map<string, string> &mimTypes) {
+    std::string extension = filePath.substr(filePath.find_last_of(".") );
+    std::string M = mimTypes[extension];
+    std::ifstream file(filePath);
+
+    if (M.empty()) {
+        statusCode = 415;
+    }
+    else if (!file.is_open()) {
+        statusCode = 404;
+    }
+    else {
+        statusCode = 200;
+    }
+
+    file.close();
+}
+
+int Response::getStatusCode() const {
+    return statusCode;
+}
+
+void Response::setContentLength(string &path) {
+
+    std::ifstream file_stream(path, std::ios::binary);
+    stringstream string_stream;
+    string_stream << file_stream.rdbuf();
+
+    //get file size
+    file_stream.seekg(0, std::ios::end);
+    content_length = file_stream.tellg();
+    file_stream.seekg(0, std::ios::beg);
+
+    file_stream.close();
+}
+
+void Response::setHeaders() {
+    headers["Content-Type"] = content_type;
+    headers["Content-Length"] = std::to_string(content_length);
+}
+
+const stringMap_t& Response::getHeaders() const {
+    for (std::map<string, string>::const_iterator it = headers.begin(); it != headers.end(); ++it) {
+        std::cout << it->first << " : " << it->second << std::endl;
+    }
+    return headers;
+}
+void Response::setContentType(const string& extension, std::map<string, string> &mimTypes) {
+
+    content_type =  mimTypes[extension];
+
+}
+
+size_t Response::getContentLength() const{
+    return content_length;
+}
+const string& Response::getContentType() const{
+    return content_type;
+}
---
+++ srcs/response/Response.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include "typedefs.hpp"
+#include "iostream"
+
+class Response {
+private:
+    int statusCode;
+    string protocol;
+    string version;
+    string body;
+    size_t content_length;
+    string content_type;
+    std::map<string, string> headers;
+    std::map<int, string> http_errors;
+
+
+
+public:
+    Response();
+    ~Response();
+
+    void setStatusCode(const string& filePath, std::map<string, string> &mimTypes);
+    void setProtocol(const string& protocol);
+    void setVersion(const string& version);
+    void setBody(const string& body);
+    void setHeaders(const stringMap_t& headers);
+    void setContentLength(string &path);
+    void setContentType(const string& filePath, std::map<string, string> &mimTypes);
+    void setHeaders();
+
+    int getStatusCode() const;
+    const string& getProtocol() const;
+    const string& getVersion() const;
+    const string& getBody() const;
+    const stringMap_t& getHeaders() const;
+    size_t getContentLength() const;
+    const string& getContentType() const;
+
+    void printData() const;
+};
