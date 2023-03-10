4f3a49a650003821ee879e7b7afc5a87dd733cf6
Fathiya Jamia <fathiyajamaa051@gmail.com>
2023-03-10 18:14:32 +0100

    add test programs

--- Playground/testGet.cpp
+++ Playground/testGet.cpp
@@ -186,7 +186,7 @@ int main(int argc, char const *argv[]) {
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(argv[1] ? atoi(argv[1]) : PORT);
 
     if (bind(serverSocket, (struct sockaddr *)&address, sizeof(address))<0) {
         perror("bind failed");
