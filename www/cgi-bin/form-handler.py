b903b3691ac08464c062ae67a274188e389ae123
Fathiya Jamia <fathiyajamaa051@gmail.com>
2023-03-14 17:44:50 +0100

    add CGI form hanlder

---
+++ www/cgi-bin/form-handler.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import cgi
+
+# Set the content type to HTML
+print("Content-Type: text/html")
+print()
+
+# Get the form data from the request
+form = cgi.FieldStorage()
+
+# Extract the form field values
+name = form.getvalue('name')
+email = form.getvalue('email')
+message = form.getvalue('message')
+
+# Print out the form data
+print("<html>")
+print("<body>")
+print("<h1>Form Data</h1>")
+print("<p>Name: %s</p>" % name)
+print("<p>Email: %s</p>" % email)
+print("<p>Message: %s</p>" % message)
+print("</body>")
+print("</html>")
