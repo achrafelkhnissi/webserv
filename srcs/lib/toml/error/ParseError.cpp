fd19c5fc2186a2692f54bdcc0baccebc7ca8a9f8
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-03-21 09:36:31 +0000

    reform: removing everything and keeping only the playground files

--- srcs/lib/toml/error/ParseError.cpp
+++
@@ -1,8 +0,0 @@
-#include "ParseError.hpp"
-
-ParseError::ParseError() { }
-
-ParseError::ParseError(std::string message, int line) {
-	this->message = message;
-	this->line = line;
-}
--- srcs/lib/toml/toml.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "data/table.hpp"
-using namespace std;
-
-namespace toml {
-table* parse_stream(ifstream&);
-table* parse_file(std::string&);
-} // namespace toml
